dd3afab9ff53bea0b3fef207adca882e1f161eca
ThatonemusicianF <120556060+ThatonemusicianF@users.noreply.github.com>
2025-04-10 00:14:53 +1200

    fixed and comments added :D

--- src/Network (WIP)/custom_network_stack.h
+++ src/Network (WIP)/custom_network_stack.h
@@ -3,10 +3,16 @@
 
 #include <stddef.h>
 
-// function declarations for the custom network stack
-int custom_initialize_interface(const char *interface, const char *ip_address, const char *netmask, const char *gateway);
-int custom_configure_dns(const char *primary_dns, const char *secondary_dns);
+// Enable a network interface
+void custom_initialize_interface(const char *interface, const char *ip_address, const char *netmask, const char *gateway);
+
+// Config DNS servers
+void custom_configure_dns(const char *primary_dns, const char *secondary_dns);
+
+// Resolve a hostname to an IP address
 int custom_resolve_hostname(const char *hostname, char *resolved_ip, size_t ip_size);
-int custom_start_server(const char *protocol, int port);
+
+// Start a TCP or UDP server
+void custom_start_server(const char *protocol, int port);
 
 #endif // CUSTOM_NETWORK_STACK_H
